ad28b2b096d2c1f72a1e49124f869c1952cd8d75
Andrew Chanthakoun <chanthakoun2002@gmail.com>
2025-07-07 15:38:45 -0700

    fetch playlist names

--- main.py
+++ main.py
@@ -9,11 +9,28 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     client_id=os.getenv('CLIENT_ID'),
     client_secret=os.getenv('CLIENT_SECRET'),
     redirect_uri=os.getenv('REDIRECT_URI'),
-    scope=os.getenv('SCOPE')
+    scope=os.getenv('SCOPE'),
+    cache_path=".spotify_cache"
 ))
 
 # user = sp.current_user()
 # print(f"Logged in as: {user['display_name']}")
 
-playlist_id = input("enter id").strip()
+playlist_id = input("Enter Playlist Link (Or Enter Playlist ID)").strip()
 
+
+#handles link into id
+if "spotify.com" in playlist_id:
+    parts = playlist_id.split("/")
+    playlist_id = parts[-1].split("?")[0] #gets the last val which will be the id
+
+# fetch and extract playlist name
+print(playlist_id)
+
+try:
+    playlist = sp.playlist(playlist_id)
+    playlist_name = playlist["name"]
+    print("Exporting: " + playlist_name)
+except Exception as e:
+    print("Error fetching playlist: ", e)
+    exit()
